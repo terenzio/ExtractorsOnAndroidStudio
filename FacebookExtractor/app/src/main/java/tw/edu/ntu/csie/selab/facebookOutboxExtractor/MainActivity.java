8da9bc13a97b31e03a7573ff421390da8a23792c
carson <r03944040@csie.ntu.edu.tw>
2014-12-03 17:33:50 +0800

    Intial FacebookExtractor project

---
+++ FacebookExtractor/app/src/main/java/tw/edu/ntu/csie/selab/facebookOutboxExtractor/MainActivity.java
@@ -0,0 +1,57 @@
+package tw.edu.ntu.csie.selab.facebookOutboxExtractor;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Base64;
+import android.util.Log;
+
+
+public class MainActivity extends FragmentActivity {
+    private MainFragment mainFragment;
+
+
+    //loads the layout in the main activity
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        printHashKey();
+        if (savedInstanceState == null) {
+            // Add the fragment on initial activity setup
+            mainFragment = new MainFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(android.R.id.content, mainFragment)
+                    .commit();
+        } else {
+            // Or set the fragment from restored state info
+            mainFragment = (MainFragment) getSupportFragmentManager()
+                    .findFragmentById(android.R.id.content);
+        }
+    }
+
+    //Generates the hash key used for Facebook console to register app.
+    public void printHashKey(){
+        // Add code to print out the key hash
+        try {
+            PackageInfo info = getPackageManager().
+                    getPackageInfo("tw.edu.ntu.csie.selab.facebookoutboxextractor",
+                            PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("HashKey:",
+                        Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (NameNotFoundException e) {
+        } catch (NoSuchAlgorithmException e) {
+        }
+    }
+
+}
